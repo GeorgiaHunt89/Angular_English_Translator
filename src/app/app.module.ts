95209753688e9c21ef0ec2407f0a4b2eac492edd
Georgia Hunt <georgialhunt89@gmail.com>
2022-02-10 22:14:18 +1030

    Comments added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+// Module
+// Defines a set of services and components that handle all the functionality for a part of the app
+
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
